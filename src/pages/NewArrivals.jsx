d94867593ee1222c41a2e077bbe2af77377e3059
supunyasantha <supunyasantha.official@gmail.com>
2025-12-13 14:23:20 +0530

    Add customize Header

--- src/pages/NewArrivals.jsx
+++ src/pages/NewArrivals.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Header from "../components/Header";
 
+import Header6 from "../assets/images/Header6.png";
+
 export default function NewArrivals() {
   return (
     <div>
-      <Header />
+      <Header
+        images={[Header6]}
+        autoSlide={false}
+        height="h-[600px] md:h-[800px]"
+      />
     </div>
   );
 }
--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
+import Header7 from "../assets/images/Header7.png";
 
 export default function Sales() {
   return (
     <div>
-      <Header />
+      <Header
+        images={[Header7]}
+        autoSlide={false}
+        height="h-[600px] md: h-[800px]"
+      />
     </div>
   );
 }
