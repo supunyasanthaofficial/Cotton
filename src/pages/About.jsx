00795b80499a52fde61b8194dca476e83e370bd1
supunyasantha <supunyasantha.official@gmail.com>
2025-12-15 14:36:24 +0530

    Add Header image size.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../components/Header";
+import Header5 from "../assets/images/Header5.png";
 
 export default function About() {
   return (
-    <div>
-      <Header />
+    <div className="relative">
+      <Header
+        images={[Header5]}
+        autoSlide={false}
+        height="h-[600px] md:h-[800px]"
+      />
     </div>
   );
 }
