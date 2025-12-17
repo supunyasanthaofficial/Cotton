81f988c27cfee27aa41e5a7ade0c1e29e07cf0f5
supunyasantha <supunyasantha.official@gmail.com>
2025-12-17 15:32:14 +0530

    Implemeting a Starting Section.

--- src/pages/Fashions.jsx
+++ src/pages/Fashions.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import Header from "../components/Header";
 
 export default function Fashions() {
   return (
     <div>
-      <Header />
+      <section className="max-w-7xl mx-auto px-4 py-12 text-center">
+        <h2 className="text-3xl md:text-4xl font-bold tracking-wide">
+          Join Our Fashion Club
+        </h2>
+        <p
+          className="max-w-3xl mx-auto
+         text-black text-xm text-bold mt-4"
+        >
+          Sign up for exclusive offers new arrivals and style inspiration.Cute
+          fashion starts here-bright colors,soft materials,and feel-good styles
+          made for women,men,and little ones.Simple,sweet,and always stylish.
+        </p>
+      </section>
     </div>
   );
 }
