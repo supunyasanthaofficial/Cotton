ee40642ea5d03e8173a315bb61acc365ad696d00
supunyasantha <supunyasantha.official@gmail.com>
2025-12-18 13:33:31 +0530

    Call the FashionproductSection, BestSalesSection and import them

--- src/pages/Fashions.jsx
+++ src/pages/Fashions.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import FashionProductSection from "../components/FashionProductSection";
+import BS1 from "../assets/images/BS1.png";
+import BestSalesSection from "../components/BestSalesSection";
 
 export default function Fashions() {
   return (
-    <div>
-      <section className="max-w-7xl mx-auto px-4 py-12 text-center">
-        <h2 className="text-3xl md:text-4xl font-bold tracking-wide">
-          Join Our Fashion Club
-        </h2>
-        <p
-          className="max-w-3xl mx-auto
-         text-black text-xm text-bold mt-4"
-        >
-          Sign up for exclusive offers new arrivals and style inspiration.Cute
-          fashion starts here-bright colors,soft materials,and feel-good styles
-          made for women,men,and little ones.Simple,sweet,and always stylish.
-        </p>
+    <>
+      <section className="w-full bg-white py-20">
+        <div className="max-w-5xl mx-auto px-4 text-center">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Join Our Fashion Club
+          </h2>
+
+          <p className="max-w-3xl mx-auto text-gray-600 text-sm md:text-base leading-relaxed mb-10">
+            Sign up for exclusive offers, new arrivals and style inspiration.
+            Cute fashion starts here — bright colours, soft materials, and
+            feel-good styles made for women,men,and little ones.Simple,sweet,and
+            always stylish.
+          </p>
+
+          {/* Email + Button */}
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="w-full sm:w-[420px] px-6 py-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500 shadow-sm"
+            />
+
+            <button className="bg-red-700 hover:bg-red-800 text-white px-10 py-3 rounded-full font-medium transition">
+              Subscribe
+            </button>
+          </div>
+        </div>
       </section>
-    </div>
+      <section>
+        <div>
+          <FashionProductSection />
+        </div>
+      </section>
+      <section>
+        <div>
+          <BestSalesSection />
+        </div>
+      </section>
+    </>
   );
 }
